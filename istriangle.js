75c043d75929bafc65b859e6584f5360ea027928
Kaivalya Nandurkar <kaivalya.nandurkar2@gmail.com>
2021-09-19 22:57:03 +0530

    initial commit

---
+++ istriangle.js
@@ -0,0 +1,26 @@
+const inputs = document.querySelectorAll(".angle-input");
+const isTriangleBtn = document.querySelector("#is-triangle-btn");
+const outputE1 = document.querySelector("#output");
+
+function calculateSumOfAngles(angle1, angle2, angle3){
+    const sumOfAngles = angle1 + angle2 + angle3;
+    //console.log(sumOfAngles);
+    return sumOfAngles; 
+}
+
+
+function isTriangle() {
+
+    const sumOfAngles = calculateSumOfAngles(Number(inputs[0].value), Number(inputs[1].value), Number(inputs[2].value));
+
+    if (sumOfAngles===180){
+        outputE1.innerText= "Yayy, the angles form a triangle."
+    } else{
+        outputE1.innerText= "Oh! The angles don't form a triangle."
+    }
+  
+
+}
+
+
+isTriangleBtn.addEventListener("click", isTriangle);
