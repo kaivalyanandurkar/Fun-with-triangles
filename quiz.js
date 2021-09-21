0310a19e5f7a3550171f6507fc13cb63a1998905
Kaivalya Nandurkar <kaivalya.nandurkar2@gmail.com>
2021-09-21 12:41:35 +0530

    docs: added few quiz questions

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ const quizForm = document.querySelector(".quiz-form");
 const submitAnswerBtn = document.querySelector("#submit-answer-btn");
 const outputE1 = document.querySelector("#output");
 
-const correctAnswers = ["90 deg", "Right angled"];
+const correctAnswers = ["90 deg", "Right angled", "28cm","69.65sqm","Incenter"];
 
 function calculateScore(){
     let score = 0;
