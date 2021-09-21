c2548c9e310da3accb3d9b95600c04d300109489
Kaivalya Nandurkar <kaivalya.nandurkar2@gmail.com>
2021-09-21 15:01:30 +0530

    fix: user can input only positive values

--- istriangle.js
+++ istriangle.js
@@ -10,14 +10,21 @@ function calculateSumOfAngles(angle1, angle2, angle3){
 
 
 function isTriangle() {
-
+    if((Number(inputs[0].value)>0 && Number(inputs[1].value)>0 && Number(inputs[2].value))){
     const sumOfAngles = calculateSumOfAngles(Number(inputs[0].value), Number(inputs[1].value), Number(inputs[2].value));
 
     if (sumOfAngles===180){
         outputE1.innerText= "Yayy, the angles form a triangle."
-    } else{
-        outputE1.innerText= "Oh! The angles don't form a triangle."
     }
+
+     else{
+        outputE1.innerText= "Oh! The angles don't form a triangle."
+    } 
+
+}
+else{
+        outputE1.innerText="Please enter valid inputs."
+    };
   
 
 }
